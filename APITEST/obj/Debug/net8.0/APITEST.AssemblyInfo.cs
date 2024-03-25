003f8fd3ae69b04644e41e39bfe764b845fcf0ce
fullstack-Maestro <furkatovichj@gmail.com>
2024-03-25 06:50:38 +0500

    first commit

--- APITEST/obj/Debug/net8.0/APITEST.AssemblyInfo.cs
+++ APITEST/obj/Debug/net8.0/APITEST.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APITEST")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2dcd3d4161bf4a51ae964171c9b250699e715506")]
 [assembly: System.Reflection.AssemblyProductAttribute("APITEST")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APITEST")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
